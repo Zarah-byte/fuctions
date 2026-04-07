46a6cb2d065d94700cd8cfcb29af39495dcf4e1a
ZarahYaqub <yaquz701@newschool.edu>
2026-04-07 13:46:42 -0400

    defined of IDs/names in array

---
+++ assets/main.js
@@ -0,0 +1,20 @@
+let sections = [
+	'cover',
+	'goal-title',
+	'reward-title',
+	'goal-number',
+	'goal-icon',
+	'goal-color',
+	'card-color'
+];
+
+let nextbutton =[
+	'step-01',
+	'step-02',
+	'step-03',
+	'step-04',
+	'step-05',
+	'step-06'
+]
+
+let backButtons = document.querySelectorAll ('[section="Back"]')
